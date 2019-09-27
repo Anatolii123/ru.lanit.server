49ab2906aad8019b9d247e141e817b930ed03aa7
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-27 13:14:11 +0500

    FirstServlet1Commit

--- src/main/java/ru/lanit/server/MainServlet.java
+++ src/main/java/ru/lanit/server/MainServlet.java
@@ -5,15 +5,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
-    }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
     }
 }
